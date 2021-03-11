dd8ff3128c0903c3c7689ab5f85f9313a0bfca1c
Costache Tudor <tudor1243@gmail.com>
2021-03-12 00:40:21 +0200

    First Batch of projects

---
+++ Java/BitcoinMiner/src/proj/Main.java
@@ -0,0 +1,18 @@
+package proj;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+            BitcoinMiner game = new BitcoinMiner();
+            game.play();
+            playAgain = BitcoinMiner.getYesOrNo("Would you like to play again?");
+        }
+
+        System.out.println("Goodbye");
+    }
+}
